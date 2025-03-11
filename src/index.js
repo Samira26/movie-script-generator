d6c887ac2b8276336d3359b3bcbc28194c118976
Samira26 <samira.mo85@gmail.com>
2025-03-11 16:12:25 +0100

    Added the basic form handling and added typewriter.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("welcome");
+function generateMovieScript(event) {
+  event.preventDefault();
+
+  new Typewriter("#movie", {
+    strings: "Generating your oscar worthy movie script.....",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let movieFormElement = document.querySelector("#movie-generator-form");
+movieFormElement.addEventListener("submit", generateMovieScript);
