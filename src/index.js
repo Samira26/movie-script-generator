696993bcd45eede8b7c734febbc79e8fcf7bc689
Samira26 <samira.mo85@gmail.com>
2025-03-12 13:58:55 +0100

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -15,11 +15,15 @@ function generateMovieScript(event) {
   let movieTheme = document.querySelector("#movie-theme");
   let apiKey = "df24oeedc433a37t0bf85c483b145ecb";
   let context =
-    "You are academy award winning screenwiter and expert in writing short films in all genres. Your mission is to generate a complete movie script for a short film in 20 lines in basic HTML(don't display the code tag). Make sure to follow the movie theme and incorporate the theme in the script.";
-  let prompt = `Movie theme: ${movieTheme.value}. Generate a movie script for a short film with a title name and character names.`;
+    "You are academy award winning screenwiter and expert in writing short films for all genres. Your mission is to generate a complete movie script for a short film in only 15 lines in basic HTML(don't display the code tag). Make sure to follow the movie theme and incorporate the theme in the script.";
+  let prompt = `Movie theme: ${movieTheme.value}. Generate a movie script for a short film with a title name and character names. The script should have a beginning, middle and an end.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating movie scrip");
+  let movieScriptElement = document.querySelector("#movie");
+  movieScriptElement.classList.remove("hidden");
+  movieScriptElement.innerHTML = `<div class="generating">🎬 Generating a short film script about ${movieTheme.value}...</div>`;
+
+  console.log("generating movie script");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
   axios.get(apiUrl).then(displayMovie);
